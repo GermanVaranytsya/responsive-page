0cb5307ebbf8301640ef43b4d8b15889f25ea3e1
German Varanytsya <s12943@pjwstk.edu.pl>
2018-08-06 18:54:01 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,1 @@
+var express = require('express');var app = express();app.use(express.static(__dirname + '/public'));app.use(express.static(__dirname + '/node_modules'));app.get('/', function (req, res) {    res.sendFile(__dirname + '/index.html');});app.listen(3000, function () {    console.log('Example app listening on port 3000!');});
